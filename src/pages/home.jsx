b814cdf4735819f7179bc14c297aa01c5e5e4e90
Shivansh Mitra <shivanshmitra53@gmail.com>
2024-05-31 05:15:27 +0530

    latest event

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -121,6 +121,7 @@ const Home = () => {
             timeStyle: "short",
             timeZone: tz,
           }),
+          bodyPreview: events.bodyPreview,
         }));
     
         setCalendarEvents(cal_eventsjson);
