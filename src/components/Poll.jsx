4c07d237fe638c87ea9e6168ceed4455a4f5f71a
Shivansh Mitra <shivanshmitra53@gmail.com>
2024-05-31 04:00:53 +0530

    poll

--- src/components/Poll.jsx
+++ src/components/Poll.jsx
@@ -1,16 +1,24 @@
+import { useState } from 'react';
+
 const Poll = () => {
+  const [selectedOption, setSelectedOption] = useState('');
+  
   const options = [
     { label: "Option One", value: "option1" },
     { label: "Option Two", value: "option2" },
     { label: "Option Three", value: "option3" },
   ];
 
+  const handleOptionChange = (value) => {
+    setSelectedOption(value);
+  };
+
   return (
-    <div className="w-[480px] min-h-[424px] bg-white rounded-lg overflow-hidden shadow-md">
-      <div className="bg-[#50917F] w-full h-[64px] flex justify-between items-center px-[30px] py-[20px] text-[white]  mb-[30px]">
+    <div className="w-full min-h-[424px] bg-white rounded-lg overflow-hidden shadow-md">
+      <div className="bg-[#50917F] w-full h-[64px] flex justify-between items-center px-[30px] py-[20px] text-white mb-[30px]">
         <h1 className="text-[20px]">Poll</h1>
         <a
-          className="text-[14px] px-[10px] py-[5px] text-[#50917F] bg-white rounded-md"
+          className="text-[14px] px-[10px] py-[5px] border-[1px] border-white rounded-[8px]"
           href=""
         >
           View All
@@ -28,17 +36,20 @@ const Poll = () => {
           {options.map((option) => (
             <div
               key={option.value}
-              className="w-full h-[62px] px-[33px] border-[1px] border-[#50917F] rounded-lg flex items-center gap-[16px]"
+              className={`w-full h-[62px] px-[33px] border-[1px] border-[#50917F] rounded-lg flex items-center gap-[16px]`}
+              onClick={() => handleOptionChange(option.value)}
             >
               <input
-                className="hidden custom-radio"
+                className="hidden"
                 type="radio"
                 id={option.value}
                 name="options"
                 value={option.value}
+                checked={selectedOption === option.value}
+                onChange={() => handleOptionChange(option.value)}
               />
-              <span className="w-[33.31px] h-[33px] border-[1px] border-[#888888] rounded-full flex items-center justify-center custom-radio-circle">
-                <span className="w-3 h-3 bg-[#3B729C] rounded-full opacity-0"></span>
+              <span className="w-[33.31px] h-[33px] border-[1px] border-[#888888] rounded-full flex items-center justify-center">
+                <span className={`w-[60%] h-[60%] bg-[#3B729C] rounded-full ${selectedOption === option.value ? 'opacity-100' : 'opacity-0'}`}></span>
               </span>
 
               <label className="text-[16px]" htmlFor={option.value}>
