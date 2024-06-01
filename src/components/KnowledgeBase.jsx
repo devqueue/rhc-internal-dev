26094354fd1bea155a35f5db231a3955c6532321
Shivansh Mitra <shivanshmitra53@gmail.com>
2024-06-02 00:51:33 +0530

    dflip

--- src/components/KnowledgeBase.jsx
+++ src/components/KnowledgeBase.jsx
@@ -38,7 +38,7 @@ const KnowledgeBase = ({ pdfs }) => {
         href = "/pdf"
           key={index}
           className="flex self-stretch p-[12px] px-[20px] mx-[20px] items-center gap-[10px] rounded-[8px] border border-[#595959] cursor-pointer"
-          onClick={() => handlePdfClick(pdf)}
+          // onClick={() => handlePdfClick(pdf)}
         >
           <img src="/icons/WebinarIcon.svg" alt="" />
 
