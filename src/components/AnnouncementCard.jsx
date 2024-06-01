50faf5943ebe425ef608b60201359f183ac3998a
i-am-SnehaChauhan <sneha2003chauhan@gmail.com>
2024-06-01 10:01:52 +0530

    Annoucement Page Complete

--- src/components/AnnouncementCard.jsx
+++ src/components/AnnouncementCard.jsx
@@ -2,21 +2,28 @@ import React from "react";
 
 const AnnouncementCard = () => {
   return (
-    <div className="overflow-hidden w-full border-b-[1px] border-white p-4 ">
-      <img
-        src="/images/announcement1.svg"
-        alt="announcement"
-        className="w-full object-cover rounded-[8px] overflow-hidden"
-      />
-      <div className="flex flex-row justify-between items-center mt-4">
-        <h1 className=" font-figtree text-[20px] font-medium text-white leading-6">RHC goes public</h1>
-        <a href="link-to-more-info" className="flex items-center text-blue-500">
-          <img
-            src="/icons/link-icon.svg"
-            alt="Read more icon"
-            className="w-]24.4px] h-[24.4px] object-cover overflow-hidden"
-          />
-        </a>
+    <div className="overflow-hidden w-full p-4 ">
+      <div className="w-full border-b-[1px] border-white">
+        <img
+          src="/images/announcement1.svg"
+          alt="announcement"
+          className="w-full object-cover rounded-[8px] overflow-hidden"
+        />
+        <div className="flex flex-row justify-between items-center mt-4 mb-3">
+          <h1 className=" font-figtree text-[20px] font-medium text-white leading-6">
+            RHC goes public
+          </h1>
+          <a
+            href="link-to-more-info"
+            className="flex items-center text-blue-500"
+          >
+            <img
+              src="/icons/link-icon.svg"
+              alt="Read more icon"
+              className="w-]24.4px] h-[24.4px] object-cover overflow-hidden"
+            />
+          </a>
+        </div>
       </div>
     </div>
   );
