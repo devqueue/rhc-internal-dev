777d3262b9871025de6286fb0eca519ba3732780
Rohit-Singh-07 <rohitsingh234@lpu.in>
2024-06-01 21:03:11 +0530

    Add links to quick links

--- src/ArabicComponents/QuickLinks.jsx
+++ src/ArabicComponents/QuickLinks.jsx
@@ -1,38 +1,40 @@
+import { Link } from "react-router-dom";
+
+
 const QuickLinks = () => {
   const links = [
-    { src: "/icons/ERPIconAr.svg", alt: "ERP Icon", label: "ERP" },
+    { src: "/icons/ERPIconAr.svg", alt: "ERP Icon",  path: "https://arhc.operations.eu.dynamics.com/"},
     {
       src: "/icons/RealEstateIconAr.svg",
       alt: "Real Estate",
-      label: "Real Estate",
+      path: "https://propertyprod.crm4.dynamics.com/main.aspx"
     },
-    { src: "/icons/DMSAr.svg", alt: "DMS" },
+    { src: "/icons/DMSAr.svg", alt: "DMS", path: "http://svrshamil/"},
     {
       src: "/icons/SelfServiceIconAr.svg",
       alt: "Employee self-service",
-      label: "Employee self-service portal",
+      path: "https://hress.arhc.com.sa:9091/"
     },
-    { src: "/icons/HelpIconAr.svg", alt: "Help Desk", label: "Help Desk" },
-    { src: "/icons/MidanAr.svg" },
-    { src: "/icons/BookRoomsAr.svg", alt: "Book rooms and meetings" },
-    { src: "/icons/TrainingAr.svg", alt: "Training" },
-    { src: "/icons/Realestateportalar.svg", alt: "Real estate portal" },
+    { src: "/icons/HelpIconAr.svg", alt: "Help Desk",  path: "http://helpdesk.riyadhholding.sa/support-portal/"},
+    { src: "/icons/MidanAr.svg", path: "https://arhc-op-web.azurewebsites.net/violations" },
+    { src: "/icons/BookRoomsAr.svg", alt: "Book rooms and meetings", path: "https://arhc.provizit.com/"},
+    { src: "/icons/TrainingAr.svg", alt: "Training", path: "https://wevolve.percipio.com/"},
+    { src: "/icons/Realestateportalar.svg", alt: "Real estate portal", path: "https://propertyproduction.powerappsportals.com/ar-SA/"},
   ];
   return (
     <div className="w-full bg-white">
       <h1>روابط سريعة</h1>
       <div className="w-full flex md:justify-center justify-start gap-[20px] flex-wrap mt-[20px]">
-        {links.map((link, index) => (
-          <div
-            key={index}
-            className="md:w-[7vw] md:h-[7vw] w-[15vw] h-[15vw] rounded-lg flex flex-col justify-center gap-[12px] items-center"
-          >
-            <img
-              className="w-full h-full object-contain"
-              src={link.src}
-              alt={link.alt}
-            />
-          </div>
+      {links.map((link, index) => (
+          <Link key={index} to={link.path} target="_blank">
+            <div className="md:w-[7vw] md:h-[7vw] w-[15vw] h-[15vw] rounded-lg flex flex-col justify-center gap-[12px] items-center">
+              <img
+                className="w-full h-full object-contain"
+                src={link.src}
+                alt={link.alt}
+              />
+            </div>
+          </Link>
         ))}
       </div>
     </div>
--- src/components/QuickLinks.jsx
+++ src/components/QuickLinks.jsx
@@ -1,38 +1,39 @@
+import { Link } from "react-router-dom";
+
 const QuickLinks = () => {
   const links = [
-    { src: "/icons/ERPIcon.svg", alt: "ERP Icon", label: "ERP" },
+    { src: "/icons/ERPIcon.svg", alt: "ERP Icon",  path: "https://arhc.operations.eu.dynamics.com/"},
     {
       src: "/icons/RealEstateIcon.svg",
       alt: "Real Estate",
-      label: "Real Estate",
+      path: "https://propertyprod.crm4.dynamics.com/main.aspx"
     },
-    { src: "/icons/DMS.svg", alt: "DMS" },
+    { src: "/icons/DMS.svg", alt: "DMS", path: "http://svrshamil/"},
     {
       src: "/icons/SelfServiceIcon.svg",
       alt: "Employee self-service",
-      label: "Employee self-service portal",
+      path: "https://hress.arhc.com.sa:9091/"
     },
-    { src: "/icons/HelpIcon.svg", alt: "Help Desk", label: "Help Desk" },
-    { src: "/icons/Midan.svg" },
-    { src: "/icons/BookRooms.svg", alt: "Book rooms and meetings" },
-    { src: "/icons/Training.svg", alt: "Training" },
-    { src: "/icons/Realestateportal.svg", alt: "Real estate portal" },
+    { src: "/icons/HelpIcon.svg", alt: "Help Desk",  path: "http://helpdesk.riyadhholding.sa/support-portal/"},
+    { src: "/icons/Midan.svg", path: "https://arhc-op-web.azurewebsites.net/violations" },
+    { src: "/icons/BookRooms.svg", alt: "Book rooms and meetings", path: "https://arhc.provizit.com/"},
+    { src: "/icons/Training.svg", alt: "Training", path: "https://wevolve.percipio.com/"},
+    { src: "/icons/Realestateportal.svg", alt: "Real estate portal", path: "https://propertyproduction.powerappsportals.com/ar-SA/"},
   ];
   return (
     <div className="w-full bg-white">
       <h1>Quick Links</h1>
       <div className="w-full flex md:justify-center justify-start gap-[20px] flex-wrap mt-[20px]">
         {links.map((link, index) => (
-          <div
-            key={index}
-            className="md:w-[7vw] md:h-[7vw] w-[15vw] h-[15vw] rounded-lg flex flex-col justify-center gap-[12px] items-center"
-          >
-            <img
-              className="w-full h-full object-contain"
-              src={link.src}
-              alt={link.alt}
-            />
-          </div>
+          <Link key={index} to={link.path} target="_blank">
+            <div className="md:w-[7vw] md:h-[7vw] w-[15vw] h-[15vw] rounded-lg flex flex-col justify-center gap-[12px] items-center">
+              <img
+                className="w-full h-full object-contain"
+                src={link.src}
+                alt={link.alt}
+              />
+            </div>
+          </Link>
         ))}
       </div>
     </div>
