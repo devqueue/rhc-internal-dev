bb62a579a911801c11a86de659299750a06721ce
i-am-SnehaChauhan <sneha2003chauhan@gmail.com>
2024-06-05 22:18:49 +0530

    Employee Directory Resposiveness Fixed

--- src/pages/EmployeeDirectory.jsx
+++ src/pages/EmployeeDirectory.jsx
@@ -1,20 +1,100 @@
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
 import Nav from "../components/Nav"
 import EmployeeCard from "../components/EmployeeCard"
 
 const EmployeeDirectory = () => {
-    const location = useLocation();
-    console.log("location", location); 
-    const employees = location.state ? location.state : [];
-    const displayedEmployees = employees ? employees.slice(0, 9) : [];
+    // const location = useLocation();
+    // console.log("location", location); 
+    // const employees = location.state ? location.state : [];
+    // const displayedEmployees = employees ? employees.slice(0, 9) : [];
+    const [events, setEvents] = useState([
+      {
+          img: "/images/employee.svg",
+          title: "Annual General Meeting",
+          jobTitle: "CEO",
+          number: "1234567890",
+          email: "Web Developer"
+      },
+      {
+          img: "/images/employee.svg",
+          title: "Annual General Meeting",
+          jobTitle: "CEO",
+          number: "1234567890",
+          email: "Web Developer"
+      },
+      {
+          img: "/images/employee.svg",
+          title: "Annual General Meeting",
+          jobTitle: "CEO",
+          number: "1234567890",
+          email: "Web Developer"
+      },
+      {
+          img: "/images/employee.svg",
+          title: "Annual General Meeting",
+          jobTitle: "CEO",
+          number: "1234567890",
+          email: "Web Developer"
+      },
+      {
+          img: "/images/employee.svg",
+          title: "Annual General Meeting",
+          jobTitle: "CEO",
+          number: "1234567890",
+          email: "Web Developer"
+      },
+      {
+          img: "/images/employee.svg",
+          title: "Annual General Meeting",
+          jobTitle: "CEO",
+          number: "1234567890",
+          email: "Web Developer"
+      },
+      {
+          img: "/images/employee.svg",
+          title: "Annual General Meeting",
+          jobTitle: "CEO",
+          number: "1234567890",
+          email: "Web Developer"
+      },
+      {
+          img: "/images/employee.svg",
+          title: "Annual General Meeting",
+          jobTitle: "CEO",
+          number: "1234567890",
+          email: "Web Developer"
+      },
+      {
+          img: "/images/employee.svg",
+          title: "Annual General Meeting",
+          jobTitle: "CEO",
+          number: "1234567890",
+          email: "Web Developer"
+      },
+      {
+          img: "/images/employee.svg",
+          title: "Annual General Meeting",
+          jobTitle: "CEO",
+          number: "1234567890",
+          email: "Web Developer"
+      },
+      {
+          img: "/images/employee.svg",
+          title: "Annual General Meeting",
+          jobTitle: "CEO",
+          number: "1234567890",
+          email: "Web Developer"
+      },
+
+  ])
   return (
     <>
     <div className="overflow-hidden w-full bg-[#F4F8FB]">
         <Nav />
       <div className="w-full pt-[30px] min-h-[424px] bg-[#F4F8FB] overflow-hidden shadow-md">
       <div className="bg-[#50917F] w-full h-[64px] rounded-[8px] rounded-bl-none rounded-br-none flex justify-between items-center px-[30px] py-[20px] text-[white] mb-[30px]">
-        <h1 className="sm:text-[20px] text-[12px]">Employee Directory</h1>
+        <h1 className="sm:text-[20px] xs:text-[16px] text-[12px]">Employee Directory</h1>
       </div>
 
       <div className="w-full flex flex-col items-start gap-[20px]">
@@ -44,15 +124,20 @@ const EmployeeDirectory = () => {
           />
         </div>
 
-        <div className="px-14 mx-auto flex flex-wrap gap-[60px] md:gap-8 lg:gap-[60px] justify-between">
-        {employees && displayedEmployees.map((employee, index) => (
+        <div className="flex flex-wrap lg:justify-start md:justify-start sm:justify-start gap-6 md:gap-8 lg:gap-[80px] lg:mx-16 md:mx-16 sm:mx-12 xs:justify-center xs:mx-4">
+        {events && events.map((event, index) => (
               <EmployeeCard
                 key={index}
-                img={`https://riyadhholding.sharepoint.com/sites/Shamil/Assets/${employee.fields.photo_name}`}
-                title={employee.fields.Title}
-                jobTitle={employee.fields.jobTitle}
-                number={employee.fields.number}
-                email={employee.fields.email}
+                // img={`https://riyadhholding.sharepoint.com/sites/Shamil/Assets/${employee.fields.photo_name}`}
+                // title={employee.fields.Title}
+                // jobTitle={employee.fields.jobTitle}
+                // number={employee.fields.number}
+                // email={employee.fields.email}
+                img={event.img}
+                title={event.title}
+                jobTitle={event.jobTitle}
+                number={event.number}
+                email={event.email}
               />
             ))}
         </div>
