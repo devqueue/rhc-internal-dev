b56fb50d9dc5678c672f628cc6e54cb14ae7ca38
i-am-SnehaChauhan <sneha2003chauhan@gmail.com>
2024-06-07 05:56:11 +0530

    language icon added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -29,11 +29,41 @@ const Nav = ({user}) => {
         {isHovered && (
           <div className="absolute top-[60px] right-0 bg-white border border-gray-300 rounded-lg shadow-md p-4">
             <p className="text-gray-800">{user.displayName}</p>
-            <p className="text-gray-500">{user.mail}</p>
+            <div className="flex flex-wrap gap-[15px] items-center justify-center ">
+        <div className="flex flex-row gap-[8px] items-center justify-center border-r-[1px] pr-2">
+          <div
+            style={{
+              backgroundImage: "url('/icons/graduation-cap.svg')",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+            className="w-[15.21px] h-[9.27px] shrink-0"
+          ></div>
+          <h2 className="text-[14px]  font-figtree font-normal leading-[21.6px]">
+            Job Title
+          </h2>
+        </div>
+        <p className="text-[14px] font-figtree font-normal leading-[21.6px]">
+            {user.jobTitle? user.jobTitle : "Web Devloper"}
+        </p>
+      </div>
           </div>
         )}
       </div>
-      <a className="text-[#3B729C]" href="/ar">العربية</a>
+      {/* <a className="text-[#3B729C]" href="/ar">العربية</a> */}
+      <div 
+        className="flex items-end justify-end  mr-4"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        onClick={() => window.location.href = "/ar"}
+      >
+        <img 
+          className="rounded-full cursor-pointer" 
+          src="images/arabic.png" 
+          alt="User Profile" 
+          style={{ width: '40px', height: '40px' }}
+        />
+        </div>
     </div>
   )
 }
