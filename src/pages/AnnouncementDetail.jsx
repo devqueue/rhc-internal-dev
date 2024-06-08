dd2806f2df4273a453ceadf583522005a6cedeb0
i-am-SnehaChauhan <sneha2003chauhan@gmail.com>
2024-06-08 12:29:07 +0530

    Announcemnet UI Fix

--- src/pages/AnnouncementDetail.jsx
+++ src/pages/AnnouncementDetail.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import Nav from "../components/Nav";
 import AnnouncementCard from "../components/AnnouncementCard";
-import { useLocation} from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const AnnouncementDetail = () => {
   const location = useLocation();
   console.log("location", location);
-  const { announcement } = location.state || {};
+  const { announcement, moreAnnouncements = [] } = location.state || {};
 
   if (!announcement) {
     return <div>Announcement not found</div>;
@@ -19,25 +19,29 @@ const AnnouncementDetail = () => {
         <div className="lg:flex pt-[30px] bg-[#F4F8FB] overflow-hidden">
           <div className="lg:w-3/4 md:w-full p-[60px] bg-white">
             <h1 className="font-figtree font-semibold text-4xl leading-[48px] text-left">
-            {announcement.fields.Title}
+              {announcement.fields.Title}
             </h1>
             <div className="flex items-center gap-[20px] mt-[40px] mb-[40px]">
               <img
-                src="/images/annoucement.svg"
+                src="/images/announcement.svg"
                 alt="announcement"
                 className="w-full h-[456px] object-cover rounded-[8px]"
               />
             </div>
 
-            <p className="font-inter font-normal text-base leading-[28px]">
-            {announcement.fields.Full_Text_en}
+            <p className="font-somar font-normal text-base leading-[32.52px]">
+              {announcement.fields.Full_Text_en}
             </p>
+            <div className="mt-6">
+            <h1 className="font-figtree font-semibold text-4xl leading-[48px] text-[#6D6D6D] text-left">Attachments</h1>
           </div>
+          </div>
+          
           <div className="lg:w-1/4 p-[60px] bg-[#50917F]">
             <h1 className="font-figtree font-semibold lg:text-4xl md:text-4xl sm:text-4xl xs:text-xl leading-[48px] text-white">
               More Announcements
             </h1>
-            {events.map((event, index) => (
+            {moreAnnouncements.map((event, index) => (
               <AnnouncementCard key={index} {...event} />
             ))}
           </div>
