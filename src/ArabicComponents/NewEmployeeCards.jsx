e59ca3b5e080c6535522e3b515982138df176be8
Haazique Sayyed <haziq.sayyed@gmail.com>
2024-06-08 21:09:25 +0530

    linkedin added to arabic

--- src/ArabicComponents/NewEmployeeCards.jsx
+++ src/ArabicComponents/NewEmployeeCards.jsx
@@ -8,23 +8,23 @@ const NewEmployeeCardsAr = () => {
   return (
     <div className="pb-[40px] w-full bg-[#F9f9f9] rounded-lg overflow-hidden">
       <div className="bg-[#3B729C] w-full h-[64px] flex justify-between items-center px-[30px] py-[20px] text-[white]">
-        <h1 className="sm:text-[20px] text-[12px]">LinkedIn</h1>
-        <Link
+        <h1 className="sm:text-[20px] text-[12px]">لينكد إن</h1>
+        {/* <Link
           to="/all-employees"
           className="sm:text-[14px] text-[9px] px-[10px] py-[5px] bg-white text-[#50917F] rounded-md"
         >
           View All
-        </Link>
+        </Link> */}
       </div>
 
-      <div className="flex gap-[41.88px] flex-wrap sm:px-[41px] px-[5vw] pt-[30px] ">
-        <NewEmployeeDirectoryAr/>
-
-        <NewEmployeeDirectoryAr/>
-
-        <NewEmployeeDirectoryAr/>
-
-        <NewEmployeeDirectoryAr/>
+      <div className="flex gap-[41.88px] flex-wrap sm:h-[800px] h-[960px] bg-white ">
+      <iframe
+        className=""
+          src="https://widgets.sociablekit.com/linkedin-page-posts/iframe/25421759"
+          frameborder="0"
+          width="100%"
+          height="1000"
+        ></iframe>
       </div>
     </div>
   );
