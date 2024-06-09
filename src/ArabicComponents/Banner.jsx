d8a285d9464326c630fd541c3ec5f6144e222e43
Haazique Sayyed <haziq.sayyed@gmail.com>
2024-06-10 01:36:46 +0300

    arabic banner updated

--- src/ArabicComponents/Banner.jsx
+++ src/ArabicComponents/Banner.jsx
@@ -3,27 +3,27 @@ const Banner = () => {
     <div className="w-full h-full relative">
       <img
         className="w-full h-full object-cover"
-        src="/images/BannerImg.png"
+        src="/images/bannerar.jpeg"
         alt=""
       />
       <div className="absolute xl:top-[0px] lg:top-[40px] sm:top-0 top-[50%] left-0 w-[30%] scale-x-[-1]">
-        <img src="/images/HeroImgPattern.png" alt="" />
+        {/* <img src="/images/HeroImgPattern.png" alt="" /> */}
       </div>
 
       <div
-        className="absolute top-[100px] lg:right-[44px] right-[5vw] lg:text-[2.3vw] sm:text-[24px] text-[16px] font-bold"
+        className="absolute top-[200px] lg:right-[44px] right-[5vw] lg:text-[2.3vw] sm:text-[24px] text-[16px] font-bold"
         style={{ fontFamily: "SomarBold" }}
       >
         <h1 className="text-white">
-          كل ما تحتاجه في مكان واحد
-          <span
+        البوابة الداخلية المطورة (شامل +)  
+          {/* <span
             className="text-['SomarLight'] font-extralight"
             style={{ fontFamily: "SomarLight" }}
-          ></span>
+          ></span> */}
         </h1>
-        <h1 className="text-white"> تعرّف على البوابة الداخلية</h1>
+        {/* <h1 className="text-white"> تعرّف على البوابة الداخلية</h1>
         <h1 className="text-white"> الجديدة شامل التي ستغير</h1>
-        <h1 className="text-white"> طريقة عملك وتواصلك</h1>
+        <h1 className="text-white"> طريقة عملك وتواصلك</h1> */}
       </div>
     </div>
   );
