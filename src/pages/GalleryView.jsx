4ee781c6f858478c185992dcf1e430df7669de6a
Rohit-Singh-07 <rohitsingh234@lpu.in>
2024-06-12 12:44:09 +0530

    update GalleryView

--- src/pages/GalleryView.jsx
+++ src/pages/GalleryView.jsx
@@ -16,34 +16,20 @@ const galleryview = () => {
             Event Gallery
           </h1>
         </div>
-        <div className="flex carousel bg-[#F4F8FB] rounded-box overflow-x-auto">
-          <div className="carousel-item lg:w-[812px] lg:h-[533px] py-[40px] pl-[40px]">
+        <div className="flex carousel bg-[#F4F8FB] rounded-box w-full justify-center items-center flex-col">
+          <div className="carousel-item sm:w-[40vw] w-[70vw] py-[40px] ">
             <img src="/images/gallery.png" alt="img" />
           </div>
-          <div className="carousel-item lg:w-[812px] lg:h-[533px] py-[40px] pl-[40px]">
-            <img src="/images/gallery.png" alt="img" />
-          </div>
-          <div className="carousel-item lg:w-[812px] lg:h-[533px] py-[40px] pl-[40px]">
-            <img src="/images/gallery.png" alt="Burger" />
-          </div>
-          <div className="carousel-item lg:w-[812px] lg:h-[533px] py-[40px] pl-[40px]">
-            <img src="/images/gallery.png" alt="Burger" />
-          </div>
-          <div className="carousel-item lg:w-[812px] lg:h-[533px] py-[40px] pl-[40px]">
-            <img src="/images/gallery.png" alt="Burger" />
-          </div>
-          <div className="carousel-item lg:w-[812px] lg:h-[533px] py-[40px] pl-[40px]">
-            <img src="/images/gallery.png" alt="Burger" />
-          </div>
-        </div>
-        <div className="lg:w-full md:w-full p-[40px] bg-white h-auto">
-          <h1 className="font-figtree font-semibold text-4xl leading-[48px] text-left">
-            {gallery.fields.Title}
-          </h1>
 
-          <p className="font-inter font-normal text-base leading-[28px]">
-            {gallery.fields.subtitle_en}
-          </p>
+          <div className="lg:w-full md:w-full p-[40px] h-auto text-center">
+            <h1 className="font-figtree font-semibold text-4xl leading-[48px]">
+              {gallery.fields.Title}
+            </h1>
+
+            <p className="font-inter font-normal text-base leading-[28px]">
+              {gallery.fields.subtitle_en}
+            </p>
+          </div>
         </div>
       </div>
     </>
