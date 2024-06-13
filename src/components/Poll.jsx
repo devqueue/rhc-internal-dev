48fa0b0b312657bccf90de67e899870b87336e9d
unknown <shivanshmitra53@gmail.com>
2024-06-13 11:25:51 +0530

    polls

--- src/components/Poll.jsx
+++ src/components/Poll.jsx
@@ -156,14 +156,14 @@ const Poll = ({ polls }) => {
                     <span>{parseInt(percentage)}%</span>
                   </div>
                   <div className="w-full bg-gray-200 rounded-full h-[24px]">
-                    <div className={`${option.id===optionIdCasted?'bg-[#50917F]':'bg-[#8f9b97]'} h-[24px] rounded-full flex justify-end pr-1`} style={{ width: `${parseInt(percentage)}%`,alignItems:'center' }}>
+                    {percentage>0 && <div className={`${option.id===optionIdCasted?'bg-[#50917F]':'bg-[#8f9b97]'} h-[24px] rounded-full flex justify-end pr-1`} style={{ width: `${parseInt(percentage)}%`,alignItems:'center' }}>
 
                       {option.id===optionIdCasted && <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-check-circle" viewBox="0 0 16 16">
                           <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                           <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/>
                         </svg>}
 
-                    </div>
+                    </div>}
                   </div>
                 </div>
               );
